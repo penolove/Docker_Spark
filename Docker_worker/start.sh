c4b313674d46c41c9c4fc68410f68df41ecdb8fe
stream <penolove15@gmail.com>
2017-04-17 18:12:09 +0800

    add env setting

--- Docker_worker/start.sh
+++ Docker_worker/start.sh
@@ -1,3 +1,7 @@
 cp /etc/hosts ~/hosts.news && sed -i "/invpm27/ s/.*/10.0.20.67\tinvpm27/g" ~/hosts.news &&\
 echo '10.0.20.70 master' >> ~/hosts.news;\
 yes | cp  ~/hosts.news /etc/hosts ;
+
+sed -i "/export SPARK_MASTER_HOST=/ s/.*/export SPARK_MASTER_HOST=master/g" /usr/local/spark/conf/spark-env.sh;
+sed -i "/export SPARK_MASTER_CORES=/ s/.*/export SPARK_MASTER_CORES=8/g" /usr/local/spark/conf/spark-env.sh;
+sed -i "/export SPARK_MASTER_MEMORY=/ s/.*/export SPARK_MASTER_MEMORY=8000m/g" /usr/local/spark/conf/spark-env.sh;
