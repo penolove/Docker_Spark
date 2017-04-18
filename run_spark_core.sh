99b80ed7f6d0c42629391a428d3a192836d85570
stream <penolove15@gmail.com>
2017-04-18 12:18:35 +0800

    add info for worker

--- run_spark_core.sh
+++ run_spark_core.sh
@@ -1,2 +1,4 @@
-docker run -h master --name spark_core \
-    -d penolove/centos7:spark_master
+docker run -h invpm27 --name spark_core \
+    -d penolove/centos7:spark_worker
+#docker run -h invpm27 --name spark_core \
+#    -d penolove/centos7:spark_worker
