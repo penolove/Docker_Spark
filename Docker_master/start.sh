79a57a72a11148710cbe7a28ff517c45b19783b7
stream <penolove15@gmail.com>
2017-04-18 10:17:58 +0800

    edit start.sh

--- Docker_master/start.sh
+++ Docker_master/start.sh
@@ -2,6 +2,6 @@ cp /etc/hosts ~/hosts.news && sed -i "/master/ s/.*/10.0.20.70\tmaster/g" ~/host
 echo '10.0.20.67 invpm27' >> ~/hosts.news;\
 yes | cp  ~/hosts.news /etc/hosts ;
 
-sed -i "/export SPARK_MASTER_HOST=/ s/.*/export SPARK_MASTER_HOST=master/g" /usr/local/spark/conf/spark-env.sh;
+sed -i "/export PARK_MASTER_HOST=/ s/.*/export SPARK_MASTER_HOST=master/g" /usr/local/spark/conf/spark-env.sh;
 sed -i "/localhost/ s/.*//g" /usr/local/spark/conf/slaves;
 echo 'invpm27'>> /usr/local/spark/conf/slaves
--- Docker_worker/start.sh
+++ Docker_worker/start.sh
@@ -2,6 +2,7 @@ cp /etc/hosts ~/hosts.news && sed -i "/invpm27/ s/.*/10.0.20.67\tinvpm27/g" ~/ho
 echo '10.0.20.70 master' >> ~/hosts.news;\
 yes | cp  ~/hosts.news /etc/hosts ;
 
-sed -i "/export SPARK_MASTER_HOST=/ s/.*/export SPARK_MASTER_HOST=master/g" /usr/local/spark/conf/spark-env.sh;
-sed -i "/export SPARK_MASTER_CORES=/ s/.*/export SPARK_MASTER_CORES=8/g" /usr/local/spark/conf/spark-env.sh;
-sed -i "/export SPARK_MASTER_MEMORY=/ s/.*/export SPARK_MASTER_MEMORY=8000m/g" /usr/local/spark/conf/spark-env.sh;
+#SPARK_MASTER_HOST acctually is this header,but I wrongly delete S in images
+sed -i "/export PARK_MASTER_HOST=/ s/.*/export SPARK_MASTER_HOST=master/g" /usr/local/spark/conf/spark-env.sh;
+sed -i "/export SPARK_WORKER_CORES=/ s/.*/export SPARK_WORKER_CORES=8/g" /usr/local/spark/conf/spark-env.sh;
+sed -i "/export SPARK_WORKER_MEMORY=/ s/.*/export SPARK_WORKER_MEMORY=8000m/g" /usr/local/spark/conf/spark-env.sh;
